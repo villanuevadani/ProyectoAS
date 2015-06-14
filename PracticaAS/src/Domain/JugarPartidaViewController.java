41487673aeddfbe4cdb142ad421c4d701480751e
Daniel Villanueva <daniel.villanueva.sanchez@est.fib.upc.edu>
2015-06-15 00:05:16 +0200

    jugarPartidaViewController

---
+++ PracticaAS/src/Domain/JugarPartidaViewController.java
@@ -0,0 +1,35 @@
+package Domain;
+
+public class JugarPartidaViewController {
+
+	public void jugarPartidaPressed(){
+		//TODO	
+	}
+	
+	public void sortirPressed(){
+		//TODO
+	}
+	
+	public void iniciarSessioPressed(String userN, String passwd){
+		//TODO
+	}
+	
+	
+	public void consultarRankingPressed(){
+		//TODO
+	}
+	
+	public void crearPartidaPressed(){
+		//TODO
+	}
+	
+	public void direccioPressed(String direccio){
+		//TODO
+	}
+	
+	public void okPressed(){
+		//TODO
+	}
+	
+	
+}
