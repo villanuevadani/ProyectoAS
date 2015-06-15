0cf9b5e610be0b51fa4da8eec04f4a09cc124822
Daniel Villanueva <daniel.villanueva.sanchez@est.fib.upc.edu>
2015-06-15 21:49:50 +0200

    pruebasNuevas

--- PracticaAS/src/Domain/pruebasNuevas.java
+++ PracticaAS/src/Domain/pruebasNuevas.java
@@ -22,13 +22,35 @@ public class pruebasNuevas {
 		
 		session.beginTransaction();
 		
-		UsuariRegistrat us = new UsuariRegistrat("villa4");
+		UsuariRegistrat us = new UsuariRegistrat("villanuevadani");
 		us.setNom("Daniel");
 		us.setCognom("Villanueva");
+		us.setPwd("davi");
+				
+		UsuariRegistrat us2 = new UsuariRegistrat("gutierrezgerman");
+		us.setNom("German");
+		us.setCognom("Gutierrez");
+		us.setPwd("gegu");
+		
+		UsuariRegistrat us3 = new UsuariRegistrat("semirajazzey");
+		us.setNom("Jazzey");
+		us.setCognom("Semira");
+		us.setPwd("jase");
+		
+		Joc2048 j2 = new Joc2048();
+		j2.setIdPartida(2048);
+		
+		Partida p = new Partida();
+		p.setEstaAcabada(true);
+		p.setEstaGuanyada(true);
+		p.setIdPartida(2048);
 		
 		
-		
+		session.save(us2);
+		session.save(us3);
 		session.save(us);
+		session.save(j2);
+		session.save(p);
 		session.getTransaction().commit();
 		
 	}
