58c314be3aaa6e3e9e211a9411bd4efeeeab4fab
jazzsemira <jazzey.semira.catapang@gmail.com>
2015-06-15 19:04:10 +0200

    +vistaRanking

--- PracticaAS/src/Domain/VistaConsultarRanking.java
+++ PracticaAS/src/Domain/VistaConsultarRanking.java
@@ -1,6 +1,8 @@
 package Domain;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,6 +10,8 @@ import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class VistaConsultarRanking extends JPanel{
 	
@@ -16,10 +20,26 @@ public class VistaConsultarRanking extends JPanel{
 	public VistaConsultarRanking(){
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.setBackground(Color.DARK_GRAY);
-		genBotons(this);
-		 VistaPrincipal.getInstance().add(this);
-	     VistaPrincipal.getInstance().pack();
 		
+		 
+	     String[] columnNames = {"Jugadors", "Millor Puntuacio"};
+	     String[][] data = {{"Aleix", "500"}, {"German", "470"}, {"Victor", "400"}, {"Dani", "250"}, {"Jazz", "150"},};
+	     
+	     JPanel jtable = new JPanel();
+	 	jtable.setLayout( new FlowLayout(FlowLayout.CENTER));
+	 	jtable.setSize(300, 300);
+	     JTable table = new JTable(data, columnNames);
+	     JScrollPane scrollPane = new JScrollPane(table);
+	     table.setPreferredScrollableViewportSize(new Dimension(500, 300));
+	     table.setSize(1500, 1500);
+	     jtable.add(table);
+	     jtable.setLayout(new BorderLayout());
+	    jtable.add(table.getTableHeader(), BorderLayout.PAGE_START);
+	    jtable.add(table, BorderLayout.CENTER);
+	    this.add(jtable);
+	     genBotons(this);
+	     VistaPrincipal.getInstance().add(this);
+	     VistaPrincipal.getInstance().pack();
 	}
 	
 	
